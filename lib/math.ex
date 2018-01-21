b7f2015d85bd969f27e7a064f62504d3398c9fbb
MainShayne233 <shaynetremblay@hotmail.com>
2018-01-21 17:57:28 -0500

    Implements `log2_star/1` function

--- lib/math.ex
+++ lib/math.ex
@@ -1,18 +1,26 @@
 defmodule Math do
-  @moduledoc """
-  Documentation for Math.
-  """
+
+  defdelegate log2(n), to: :math
 
   @doc """
-  Hello world.
+  Computes lg*(n)
 
   ## Examples
 
-      iex> Math.hello
-      :world
-
+      iex> Math.log2_star(2)
+      1
+      iex> Math.log2_star(4)
+      2
+      iex> Math.log2_star(16)
+      3
+      iex> 2 |> :math.pow(16) |>  Math.log2_star()
+      4
   """
-  def hello do
-    :world
+  def log2_star(n, count \\ 0)
+  def log2_star(n, count) when n <= 1, do: count
+  def log2_star(n, count) do
+    n
+    |> log2
+    |> log2_star(count + 1)
   end
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -1,8 +1,4 @@
 defmodule MathTest do
   use ExUnit.Case
   doctest Math
-
-  test "greets the world" do
-    assert Math.hello() == :world
-  end
 end
